cec1695f7ad8fa2a7ca49672507da9d3e4ec04eb
unknown <dumpling.lol13@gmail.com>
2026-04-30 11:00:42 +0300

    Add lib_labirint, lib_DSU

---
+++ lib_DSU/DSU.h
@@ -0,0 +1,81 @@
+#pragma once
+#include <iostream>
+#include <stdexcept>
+
+class DSU {
+	int* _parent;
+	int* _rank;
+	size_t _size;
+public:
+	DSU(size_t size);
+	~DSU();
+	void union_set(int x, int y);
+	int find(int x);
+	int find_recurtion(int x);
+	int get_rank(int x) const;
+
+};
+
+DSU::DSU(size_t size) : _size(size) {
+	if (size == 0) {
+		throw std::logic_error("The size cannot be equal to 0");
+	}
+	_parent = new int[size];
+	_rank = new int[size];
+	for (size_t i = 0; i < size; ++i) {
+		_parent[i] = i;
+		_rank[i] = 0;
+	}
+}
+DSU::~DSU() {
+	delete[] _parent;
+	delete[] _rank;
+}
+
+void DSU::union_set(int x, int y) {
+	if (x >= _size || y >= _size || x < 0 || y < 0) {
+		throw std::logic_error("Going beyond borders");
+	}
+
+	int parent_x = find(x);
+	int parent_y = find(y);
+
+	if (_rank[parent_x] < _rank[parent_y]) {
+		_parent[parent_x] = parent_y;
+
+	}
+	else if (_rank[parent_x] > _rank[parent_y]) {
+		_parent[parent_y] = parent_x;
+
+	}
+	else {
+		_parent[parent_y] = parent_x;
+
+		_rank[parent_x]++;
+	}
+}
+int DSU::find(int x) {
+	if (x >= _size || x < 0) {
+		std::cerr << "ERROR: x = " << x << ", _size = " << _size << std::endl;
+		throw std::logic_error("Going beyond borders");
+	}
+	
+	return find_recurtion(x);
+}
+int DSU::find_recurtion(int x) {
+	if (x >= _size || x < 0) {
+		std::cerr << "ERROR: x = " << x << ", _size = " << _size << std::endl;
+		throw std::logic_error("Going beyond borders");
+	}
+	if (_parent[x] == x) {
+		return x;
+	}
+	return _parent[x] = find_recurtion(_parent[x]);
+}
+
+int DSU::get_rank(int x) const {
+	if (x >= _size || x < 0) {
+		throw std::logic_error("Going beyond borders");
+	}
+	return _rank[x];
+}
